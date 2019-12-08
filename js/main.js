8c8ea46c50fcbcf683ec788935907adeaa389c1e
blackrose9 <sarah.mwendwa@gmail.com>
2019-12-08 23:39:41 +0300

    Add toggle feature to the 'what we do' section. Also made a couple of id and class name changes.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$().ready(function(){
+   $("#wwd1").click(function(){
+      $("#wwdtext1").toggle();
+      $("#wwdimage1").toggle();
+   });
+});
+$().ready(function(){
+   $("#wwd2").click(function(){
+      $("#wwdtext2").toggle();
+      $("#wwdimage2").toggle();
+   });
+});
+$().ready(function(){
+   $("#wwd3").click(function(){
+      $("#wwdtext3").toggle();
+      $("#wwdimage3").toggle();
+   });
+});
